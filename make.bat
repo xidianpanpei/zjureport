b4e0918801515d50588eaf246d4d9e6cffa0a624
panpei <xidianpanpei@126.com>
2013-11-05 00:42:50 +0800

    修改一键编译脚本

--- make.bat
+++ make.bat
@@ -2,7 +2,17 @@
 title ZJU Graduate Report
 
 call "./clean.bat"
+del *.pdf /s
+
+echo "===============>BEGAIN<==============="
 xelatex report
 bibtex report
 xelatex report
 xelatex report
+echo "================>END<================="
+
+echo "*************CLEAN START**************"
+call "./clean.bat"
+echo "**************CLEAN END***************"
+
+pause
